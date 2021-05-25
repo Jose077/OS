98d7578a0e36ad865ec9cbac0f7de2c809debce8
tharlys10 <tharlys10c@hotmail.com>
2021-05-25 11:33:32 -0300

    feat: :sparkles: create migration product

---
+++ backend/src/shared/infra/typeorm/migrations/1621953037270-CreateProduto.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateProduto1621953037270 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table(
+        {
+          name: 'produto',
+          columns: [
+            {
+              name: "id",
+              type: "uuid",
+              isPrimary: true
+            },
+            {
+              name: "nome",
+              type: "varchar",
+            },
+            {
+              name: "preco",
+              type: "numeric",
+            },
+          ]
+        }
+      )
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("produto");
+  }
+
+
+}
