7577f4e8f2dd455ffa90a85dde810c4ba4e93cd6
tharlys10 <tharlys10c@hotmail.com>
2021-05-25 11:30:26 -0300

    feat: :sparkles: create migration client

---
+++ backend/src/shared/infra/typeorm/migrations/1621952822298-CreateClient.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateClient1621952822298 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table(
+        {
+          name: 'cliente',
+          columns: [
+            {
+              name: "id",
+              type: "uuid",
+              isPrimary: true
+            },
+            {
+              name: "nome",
+              type: "varchar",
+            },
+          ]
+        }
+      )
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("cliente");
+  }
+
+}
